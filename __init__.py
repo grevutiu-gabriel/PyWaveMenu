01d2863be84f04d8dd152482ce71dfea28deab82
Helder <heldercro@gmail.com>
2015-07-19 08:56:04 -0300

    Wrapping up

--- __init__.py
+++ __init__.py
@@ -4,9 +4,11 @@
 
 import sys, os, time
 
-from PyQt4.QtGui import *
+from PyQt4.QtGui import QApplication, QMainWindow
 from PyQt4.QtCore import *
 from PyQt4.uic import *
+
+import pywt
 
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
@@ -34,10 +36,31 @@ class Main(QMainWindow):
         self.plotLayout.addWidget(self.toolbar)
         self.plotLayout.addWidget(self.canvas)
 
+        self.populateWindow()
+
         # connecting signals to slots
         self.actionQuit.triggered.connect(self.close)
+        
+        ###########################################################################
+        ###########################################################################
+        # verificar como passar uma string: novo estilo de conectar signal-slot
+        self.cbbWaveletFamily.activated.connect(self.populateCBBWavelet(str))
     # __init__()
 
+
+    def populateWindow(self):
+        self.cbbWaveletFamily.addItems(pywt.families())
+        #for family in pywt.families():
+        #    self.cbbWaveletFamily
+            #for wave in pywt.wavelist(family):
+        
+    # populateWindow(self)
+    
+    def populateCBBWavelet(self, family):
+        print('oiooo ', family)
+        self.cbbWavelet.addItems(pywt.wavelist(family))
+        
+    # populateWindow(self)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
